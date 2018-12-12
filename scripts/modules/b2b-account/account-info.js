0cac690097ed503b36aaaf816dd5374b14d053f6
Aaron Jones <Amjcraft@gmail.com>
2018-12-12 09:26:57 -0600

    start of account info

---
+++ scripts/modules/b2b-account/account-info.js
@@ -0,0 +1,18 @@
+define(["modules/jquery-mozu", 'modules/api', "underscore", "hyprlive", "modules/backbone-mozu", "hyprlivecontext"], function ($, api, _, Hypr, Backbone, HyprLiveContext) {
+
+    var InfoView = Backbone.MozuView.extend({
+        templateName: "modules/b2b-account/account-view",
+        initialize: function () {
+            Backbone.MozuView.prototype.initialize.apply(this, arguments);
+        },
+        render: function () {
+            var self = this;
+            Backbone.MozuView.prototype.render.apply(this, arguments);
+           
+        }
+    });
+
+    return {
+        'InfoView': InfoView
+    }
+});
--- scripts/modules/b2b-account/b2b-account.js
+++ scripts/modules/b2b-account/b2b-account.js
@@ -1,9 +1,12 @@
-define(["modules/jquery-mozu", 'modules/api', "underscore", "hyprlive", "modules/backbone-mozu", "hyprlivecontext", 'modules/mozu-grid/mozugrid-view', 'modules/mozu-grid/mozugrid-pagedCollection', "modules/views-paging", "modules/models-product", "modules/models-wishlist", "modules/search-autocomplete", "modules/models-cart", 'modules/models-customer', "modules/b2b-account/quotes", "modules/b2b-account/users", "modules/b2b-account/orders", "modules/b2b-account/returns", "modules/b2b-account/payment-information", "modules/backbone-pane-switcher"], function ($, api, _, Hypr, Backbone, HyprLiveContext, MozuGrid, MozuGridCollection, PagingViews, ProductModels, WishlistModels, SearchAutoComplete, CartModels, CustomerModels, Lists, Users, Orders, Returns, PaymentInformation, PaneSwitcher) {
+define(["modules/jquery-mozu", 'modules/api', "underscore", "hyprlive", "modules/backbone-mozu", "hyprlivecontext", 'modules/models-customer', "modules/b2b-account/quotes", "modules/b2b-account/users", "modules/b2b-account/orders", "modules/b2b-account/returns", "modules/b2b-account/payment-information", "modules/backbone-pane-switcher", "modules/account-info"], function ($, api, _, Hypr, Backbone, HyprLiveContext, CustomerModels, Lists, Users, Orders, Returns, PaymentInformation, PaneSwitcher, AccountInfo) {
 
     var paneSwitcherModel = new PaneSwitcher.PaneSwitcherModel({
         panes: [
             {
-                name: 'Account Information'
+                name: 'Account Information',
+                view: new AccountInfo.InfoView({
+                    model: CustomerModels.EditableCustomer.fromCurrent()
+                })
             },
             {
                 name: 'Orders',
